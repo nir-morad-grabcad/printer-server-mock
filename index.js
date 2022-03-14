e4b4b0a9243bfcb475fcbc92ee0df0f066f42b01
Nir Morad <Nir.Morad@Grabcad.com>
2022-03-14 15:44:06 +0200

    add cros

--- index.js
+++ index.js
@@ -1,78 +1,70 @@
-"use strict";
-​
-var fs = require("fs"),
-  path = require("path"),
-  http = require("http");
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http');
 var cors = require("cors");
-require("debug").enable("*");
-​
-var app = require("connect")();
-var swaggerTools = require("swagger-tools");
-var jsyaml = require("js-yaml");
+
+
+
+require('debug').enable('*');
+
+var app = require('connect')();
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
 var serverPort = 8080;
-​
+
 // swaggerRouter configuration
 var options = {
-  swaggerUi: path.join(__dirname, "/swagger.json"),
-  controllers: path.join(__dirname, "./controllers"),
-  useStubs: process.env.NODE_ENV === "development", // Conditionally turn on stubs (mock mode)
+  swaggerUi: path.join(__dirname, '/swagger.json'),
+  controllers: path.join(__dirname, './controllers'),
+  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
 };
-​
+
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync(path.join(__dirname, "api/swagger.yaml"), "utf8");
+var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
 var swaggerDoc = jsyaml.safeLoad(spec);
-​
+
 app.use(cors());
-​
 // Add headers
 app.use(function (req, res, next) {
+
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-​
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
   // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-​
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
   // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, api_key, Authorization"
-  );
-​
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, api_key, Authorization');
+  
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-​
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
   // Pass to next layer of middleware
   next();
-});
-​
+  });
+
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+
   // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
   app.use(middleware.swaggerMetadata());
-​
+
   // Validate Swagger requests
   app.use(middleware.swaggerValidator());
-​
+
   // Route validated requests to appropriate controller
   app.use(middleware.swaggerRouter(options));
-​
+
   // Serve the Swagger documents and Swagger UI
   app.use(middleware.swaggerUi());
-​
+
   // Start the server
   http.createServer(app).listen(serverPort, function () {
-    console.log(
-      "Your server is listening on port %d (http://localhost:%d)",
-      serverPort,
-      serverPort
-    );
-    console.log(
-      "Swagger-ui is available on http://localhost:%d/docs",
-      serverPort
-    );
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
   });
+
 });
